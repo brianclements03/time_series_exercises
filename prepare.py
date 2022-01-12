bf11b42793cb5084ed8ab29b68cc27bb432bf806
Brian Clements <brian.l.clements03@gmail.com>
2022-01-12 11:06:45 -0600

    add comments to .py

--- prepare.py
+++ prepare.py
@@ -16,15 +16,25 @@ plt.style.use('seaborn-whitegrid')
 plt.rc('font', size=16)
 
 def prep_store_data(df):
+    # Convert date column to datetime format.
     df.sale_date = pd.to_datetime(df.sale_date)
+    # Set the index to be the datetime variable.
     df = df.set_index("sale_date").sort_index()
+    # Add a 'month' and 'day of week' column to your dataframe.
     df['month'] = df.index.month_name()
     df['day'] = df.index.day_name()
+    # Add a column 'sales_total', which is a derived from sale_amount (total items) and item_price
     df['sales_total'] = df.sale_amount * df.item_price
-
-
+    # Plot the distribution of sale_amount and item_price.
     sns.barplot(y='sale_amount', x='item_price', data=df)
     plt.xticks(rotation = 60)
     plt.tight_layout
     
     return df
+
+
+
+
+
+
+
