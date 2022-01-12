3869e517ea2e6730bd49b12ed0979a5a1a1585df
Brian Clements <brian.l.clements03@gmail.com>
2022-01-12 15:26:30 -0600

    reorganized my TSA notebooks and redid some notes and exercises

--- prepare.py
+++ prepare.py
@@ -16,25 +16,36 @@ plt.style.use('seaborn-whitegrid')
 plt.rc('font', size=16)
 
 def prep_store_data(df):
-    # Convert date column to datetime format.
+    # Convert date column to datetime format
     df.sale_date = pd.to_datetime(df.sale_date)
     # Set the index to be the datetime variable.
     df = df.set_index("sale_date").sort_index()
+    # Plot the distribution of sale_amount and item_price
+    df[['sale_amount','item_price']].hist()
     # Add a 'month' and 'day of week' column to your dataframe.
     df['month'] = df.index.month_name()
     df['day'] = df.index.day_name()
-    # Add a column 'sales_total', which is a derived from sale_amount (total items) and item_price
-    df['sales_total'] = df.sale_amount * df.item_price
-    # Plot the distribution of sale_amount and item_price.
-    sns.barplot(y='sale_amount', x='item_price', data=df)
-    plt.xticks(rotation = 60)
-    plt.tight_layout
-    
+    # Add a 'sales_total' column, which is a derived from sale_amount (total items) and item_price
+    df['sales_total'] = df.sale_amount * df.item_price    
     return df
 
 
 
 
-
+def prep_ops_data():
+    # Acquire the raw ops data
+    df = pd.read_csv("https://raw.githubusercontent.com/jenfly/opsd/master/opsd_germany_daily.csv")
+    # Convert date column to datetime format.
+    df.Date = pd.to_datetime(df.Date)
+    # Set the index to be the datetime variable.
+    df = df.set_index("Date").sort_index()
+    # Plot the distribution of each of your variables.
+    df.hist()
+    # Add a month and a year column to your dataframe
+    df['year'] = df.index.year
+    df['month'] = df.index.month_name()
+    # Fill any missing values
+    df = df.fillna(0)
+    return df
 
 
