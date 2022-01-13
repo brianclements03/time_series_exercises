d970bb6be1b59f7f0520139ca931ebb810b836fb
Brian Clements <brian.l.clements03@gmail.com>
2022-01-13 14:31:31 -0600

    acquired and aggregated the sales data so there is only one date per observation

--- prepare.py
+++ prepare.py
@@ -17,16 +17,16 @@ plt.rc('font', size=16)
 
 def prep_store_data(df):
     # Convert date column to datetime format
-    df.sale_date = pd.to_datetime(df.sale_date)
+    df.sale_date = pd.to_datetime(df.sale_date)# consider placing some datetime format= code here to speed the processing
     # Set the index to be the datetime variable.
     df = df.set_index("sale_date").sort_index()
-    # Plot the distribution of sale_amount and item_price
-    df[['sale_amount','item_price']].hist()
     # Add a 'month' and 'day of week' column to your dataframe.
     df['month'] = df.index.month_name()
     df['day'] = df.index.day_name()
     # Add a 'sales_total' column, which is a derived from sale_amount (total items) and item_price
-    df['sales_total'] = df.sale_amount * df.item_price    
+    df['sales_total'] = df.sale_amount * df.item_price
+    # Plot the distribution of sale_amount and item_price and sales_total
+    df[['sale_amount','item_price','sales_total']].hist()
     return df
 
 
